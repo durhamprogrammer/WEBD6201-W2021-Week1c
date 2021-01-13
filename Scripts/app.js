de7cedb3e7a18a0ff902355fd7ec86038922fc01
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-01-13 09:48:46 -0500

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,21 @@
+/* custom scripts go here */
+
+//IIFE - Immediately Invoked Function Expression
+//AKA: Anonymous Self-Executing Function
+
+(function()
+{
+    // initialization function
+    function Start()
+    {
+        console.log("Have a nice day");
+    }
+    
+
+    // event listener
+    window.addEventListener("load", Start);
+
+})();
+
+
+
